4f864d54dfdfb96a85aa9e0d6ff5391a07aff17d
soultakpara <soultakparadev@gmail.com>
2026-05-08 14:16:17 +0100

    [FIX]:integration correct de speedINSights

--- Portofolio/src/main.jsx
+++ Portofolio/src/main.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from './components/App.jsx'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
-import App from './components/App.jsx'
-
 createRoot(document.getElementById('root')).render(
+  <SpeedInsights>
   <StrictMode>
     <App />
-  </StrictMode>,
+  </StrictMode>
+  </SpeedInsights>,
 )
